8cbea73dd00b3ca02a03698777f829a8b0fe91cd
ant.hakim.stud <ant.hakim.stud@gmail.com>
2021-06-08 08:59:48 -0400

    modifed ci.yml

--- python/sh/install_dependencies.sh
+++ python/sh/install_dependencies.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-python -m pip install dotenv
-python -m pip install os
-python -m pip install discord
-python -m pip install random
-python -m pip install constants
-python -m pip install helpers
-python -m pip install bs4
-python -m pip install urlopen
-python -m pip install aiohttp
-python -m pip install glob
-python -m pip install beautifulsoup4
-python -m pip install lxml
-python -m pip install requests
+pip install dotenv
+pip install os
+pip install discord
+pip install random
+pip install constants
+pip install helpers
+pip install bs4
+pip install urlopen
+pip install aiohttp
+pip install glob
+pip install beautifulsoup4
+pip install lxml
+pip install requests
